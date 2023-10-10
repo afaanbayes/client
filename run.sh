bdd748bc0e8557c19b8dfdad898fe886d06268a0
afaan <afaan@bayes.global>
2023-10-10 12:22:49 -0700

    added sepoli config

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Run the main.py command in the background
-gnome-terminal -- bash -c "python main.py --num_workers 3 --num_rounds 1; exec bash"
 
 # Define the number of workers
 num_workers=14
